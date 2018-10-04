e22a11659142eabbaf6c148779a7f583cbf705e9
Bjarne <bjarne.bulow@studentlitteratur.se>
2018-10-04 10:22:14 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
-          <Hello name={"Fiskbil"} enthusiasmLevel={25}/>
+          <Hello name={"all of you out there!"} enthusiasmLevel={25}/>
       </div>
     );
   }
